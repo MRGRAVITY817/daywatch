5e0f95ac2d7c81036a7738824820319202fdbdfd
Hoon Wee <mrgravity817@gmail.com>
2023-12-29 12:06:13 +0900

    feat: add money newtype

---
+++ daywatch-newtypes/README.md
@@ -0,0 +1,9 @@
+# daywatch_newtypes
+
+Tired of implementing commonly used value objects (or newtypes, in rust) in every projects?
+
+Do not worry - `daywatch_newtypes` has done the hard work for you.
+
+## Examples
+
+### TODO: Add examples
--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -1,3 +1,16 @@
+use daywatch_newtypes::{
+    self,
+    money::{iso, Money},
+};
+
+struct User<'a> {
+    money: Money<'a, iso::Currency>,
+}
+
 fn main() {
+    let user = User {
+        money: Money::from_major(1000, iso::KRW),
+    };
+
     println!("Hello, world!");
 }
